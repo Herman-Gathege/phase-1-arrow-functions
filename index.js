14505c4d4a520511b1f8af1bae170bc6fc77f024
Herman-Gathege <siinamtechstudio@gmail.com>
2024-07-02 09:04:21 +0300

    add change

---
+++ index.js
@@ -0,0 +1,28 @@
+function divide (divide) {
+  let a = 2000
+  let b = 100
+ 
+ if ((a, b) = (a/b)) {
+  return divide;
+ }
+}
+
+function square(square) {
+  let parameter=parameter
+  let x = 2
+
+  if (square(x)===4) {
+    return square;
+   }
+ }
+
+
+ function add(add) {
+  let parameter=parameter + parameter
+  let a = 3
+  let b = 4
+
+  if (add(a,b)=(a+b) ) {
+    return add;
+  }
+ } 
